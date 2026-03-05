af5d2e6006c55e483a4657e71a36ccd22f160b6a
betapcode17 <quocdat19991712@gmail.com>
2026-03-05 15:15:01 +0700

    feat(layout): Add main application layout
    - Implemented Header component
    - Implemented Footer component
    - Created MainLayout component
    - Integrated layout into public routes
    - Added NavigationMenu UI component

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
+
+const MainLayout = () => {
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <Header></Header>
+      <main className='flex-1'>
+        <Outlet></Outlet>
+      </main>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default MainLayout
